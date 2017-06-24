8e1655b526853af7a59c1acdf78c44ea4bfff049
Scott Blumenthal <scott.blumenthal@gmail.com>
2017-06-24 10:23:44 -0400

    stub in redux

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,8 +1,33 @@
-
 import React, { PropTypes } from 'react';
+import { createStore } from 'redux'
+import { connect, Provider } from 'react-redux'
 import styles from './styles.scss';
 
 import Nav from '../Nav'
+
+
+const makeProjectReducer = (initialState) => {
+
+  return (state=initialState, action) => {
+    // const getById = (id) => {
+    //   return { ...state.entitiesById[id] }
+    // }
+
+    let { id, key } = action
+
+    switch (action.type) {
+      case 'RESET_STORE':
+        return action.store
+
+      // case 'ADD_ENTITY':
+      //   entitiesById = addWithChildren(entity, entitiesById)
+      //   parent.entities.splice(position, 0, entity.id)
+      //   entitiesById[parentId] = parent
+      //   return {...state, entitiesById }
+
+    return state
+  }
+}
 
 function App({ children }) {
 
